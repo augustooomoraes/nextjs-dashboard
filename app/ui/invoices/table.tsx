8cdcc8c9b201772ed9e5ba671220ab49bc3364fb
Augusto Moraes <cdm.augusto@gmail.com>
2023-11-15 14:14:10 -0300

    Forgot another <Image /> alt text.

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -65,7 +65,7 @@ export default async function InvoicesTable({ query, currentPage }: { query: str
                 <tr key={invoice.id} className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg">
                   <td className="whitespace-nowrap py-3 pl-6 pr-3">
                     <div className="flex items-center gap-3">
-                      <Image src={invoice.image_url} className="rounded-full" width={28} height={28} />
+                      <Image src={invoice.image_url} className="rounded-full" width={28} height={28} alt="I don't really care. I just need something here for the project to deploy." />
                       <p>{invoice.name}</p>
                     </div>
                   </td>
